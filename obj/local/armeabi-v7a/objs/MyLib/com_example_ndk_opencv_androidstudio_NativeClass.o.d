cfa2ad121f490b9905ae2cf1a847e75ef6c11a13
Liu Chang <lllccc2009@gmail.com>
2015-10-28 22:21:56 -0400

    add some libs

---
+++ obj/local/armeabi-v7a/objs/MyLib/com_example_ndk_opencv_androidstudio_NativeClass.o.d
@@ -0,0 +1,215 @@
+/media/changliu/Research/github/ccv_android/obj/local/armeabi-v7a/objs/MyLib/com_example_ndk_opencv_androidstudio_NativeClass.o: \
+ /media/changliu/Research/github/ccv_android/jni/com_example_ndk_opencv_androidstudio_NativeClass.cpp \
+ /media/changliu/Research/github/ccv_android/jni/com_example_ndk_opencv_androidstudio_NativeClass.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/jni.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/opencv.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/assert.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdlib.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/string.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/malloc.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/alloca.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/strings.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdint.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/memory.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/math.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/limits.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/limits.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/limits.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/internal_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/limits.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/syslimits.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/page.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/flann/config.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/photo/photo.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/photo/photo_c.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/video.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/background_segm.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/list \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/calib3d/calib3d.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/ml/ml.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/contrib.hpp \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/retina.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/valarray \
+ /home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/openfabmap.hpp \
+ /media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/set
+
+/media/changliu/Research/github/ccv_android/jni/com_example_ndk_opencv_androidstudio_NativeClass.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/jni.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/opencv.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/assert.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdlib.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/string.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/alloca.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/strings.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/memory.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/math.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/limits.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/limits.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/limits.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/internal_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/limits.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/syslimits.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/page.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/limits:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/photo/photo.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/photo/photo_c.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/video.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/video/background_segm.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/list:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/calib3d/calib3d.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/ml/ml.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/iostream:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/contrib.hpp:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/retina.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/valarray:
+
+/home/changliu/OpenCV-2.4.9-android-sdk/sdk/native/jni/include/opencv2/contrib/openfabmap.hpp:
+
+/media/changliu/Research/android-ndk-r10e/sources/cxx-stl/gnu-libstdc++/4.8/include/set:
---
+++ obj/local/armeabi-v7a/objs/MyLib/media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT.o.d
@@ -0,0 +1,80 @@
+/media/changliu/Research/github/ccv_android/obj/local/armeabi-v7a/objs/MyLib//media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT.o: \
+ /media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT.c \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/string.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/malloc.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/assert.h \
+ /media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdio.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdint.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_wchar_limits.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/types.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/inttypes.h \
+ /media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT-params.h \
+ /media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT-params19937.h \
+ /media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT-common.h \
+ /media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/alloca.h
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/string.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/assert.h:
+
+/media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdio.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/inttypes.h:
+
+/media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT-params.h:
+
+/media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT-params19937.h:
+
+/media/changliu/Research/github/ccv_android/jni/ccv/lib/3rdparty/sfmt/SFMT-common.h:
+
+/media/changliu/Research/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/alloca.h:
