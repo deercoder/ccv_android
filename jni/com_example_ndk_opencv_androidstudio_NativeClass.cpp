e86911451baed50c7038ecc4d434a800fc4c8ac5
Liu Chang <lllccc2009@gmail.com>
2015-10-31 14:12:35 -0400

    add new functions for test jni

--- jni/com_example_ndk_opencv_androidstudio_NativeClass.cpp
+++ jni/com_example_ndk_opencv_androidstudio_NativeClass.cpp
@@ -10,3 +10,8 @@ JNIEXPORT jstring JNICALL Java_com_example_ndk_1opencv_1androidstudio_NativeClas
   (JNIEnv * env, jobject obj){
     return env->NewStringUTF("Hello from JNI");
 }
+
+JNIEXPORT jstring JNICALL Java_com_example_ndk_opencv_androidstudio_NativeClass_getStringFromNative
+  (JNIEnv * env, jobject obj){
+    return env->NewStringUTF("Hello from JNI");
+}
--- jni/com_example_ndk_opencv_androidstudio_NativeClass.h
+++ jni/com_example_ndk_opencv_androidstudio_NativeClass.h
@@ -15,6 +15,14 @@ extern "C" {
 JNIEXPORT jstring JNICALL Java_com_example_ndk_1opencv_1androidstudio_NativeClass_getStringFromNative
   (JNIEnv *, jobject);
 
+/*
+ * Class:     com_example_ndk_opencv_androidstudio_NativeClass
+ * Method:    getStringFromNative
+ * Signature: ()Ljava/lang/String;
+ * Function:  add my own test function
+ */
+JNIEXPORT jstring JNICALL Java_com_example_ndk_opencv_androidstudio_NativeClass_getStringFromNative
+  (JNIEnv *, jobject);
 #ifdef __cplusplus
 }
 #endif
